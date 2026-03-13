295b643a182dcaecbff6496a2407e7cf51dd6b53
Kaan <85441308+mkk776@users.noreply.github.com>
2026-03-13 19:58:41 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,17 @@
+# To run this test you need to move this test script the same path as Main.py
+from Main import *
 
+people = [
+    TeamMember("asd", 1),
+    TeamMember("asd", 1),
+    TeamMember("asd", 1),
+    TeamMember("asd", 1),
+    TeamMember("asd", 1),
+    TeamMember("asd", 1),
+]
+
+greet(people[0].name)
+print(count_members(people))
+display_team(people)
+
+print_yuri()
