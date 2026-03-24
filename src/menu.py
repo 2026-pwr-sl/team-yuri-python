44c13492a4e5c055f1a31e62ff32c15428e66a51
Elif Karakaş <karakaselif52@gmail.com>
2026-03-24 19:38:17 +0100

    Create menu.py

---
+++ src/menu.py
@@ -0,0 +1,19 @@
+def display(n=0, options=[]):
+    print("choose what to run (up and down):")
+    for i in range(len(options)):
+        if i == n:
+            print("> " + options[i])
+        else:
+            print("  " + options[i])
+
+
+options = [
+    "python3 main.py --menu",
+    "python3 main.py --show-team",
+    "python3 main.py --count",
+    "python3 main.py --greet",
+]
+
+display(1, options)
+
+# todo make it choosable
