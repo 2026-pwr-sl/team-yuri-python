ec1af3ae363c20a83b33a654adeb4b4256e5d5bb
Elif Karakaş <karakaselif52@gmail.com>
2026-05-03 19:11:14 +0200

    Update lab7.py

--- src/lab7.py
+++ src/lab7.py
@@ -10,4 +10,18 @@ def reqstr2obj(request_string):
     if not isinstance(request_string, str):
         raise TypeError("request_string must be a string")
 
-    return HTTPRequest("GET", "/", "HTTP1.1")
+    if not len(request_string.strip(' ').split(' '))==3:
+        return None
+
+    request_splitted = request_string.split(" ")
+
+    possible_request_types = ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
+    possible_http_protocols = ["HTTP0.9", "HTTP1.0", "HTTP1.1", "HTTP2", "HTTP3"]
+
+    if not request_splitted[0] in possible_request_types:
+        raise TypeError("request_type is nonexistent")
+
+    if not request_splitted[2] in possible_http_protocols:
+        raise TypeError("http_protocol is nonexistent")
+
+    return HTTPRequest(*request_splitted) # sorry for this gross line
